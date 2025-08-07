63f36c8770df8825f6e1c2a4bc9e3e9123bdec3b
antnose <antnose28@gmail.com>
2025-08-07 20:23:10 +0600

    Know about function

---
+++ JS-Function/function.js
@@ -0,0 +1,14 @@
+const price = 45;
+
+const fanOffKor = () => {
+  console.log("Bosha theke uthe darao");
+  console.log("Walk to the switch");
+  console.log("Click the switch of the fan");
+};
+
+function brushTeeth() {
+  console.log("Pickup the bruish");
+}
+
+fanOffKor();
+brushTeeth();

68b4d8d2f12ce0726c56b5facd0dfb70902abc59
antnose <antnose28@gmail.com>
2025-08-07 21:29:37 +0600

    Function parameter handle multiple perameters

---
+++ JS-Function/parameter.js
@@ -0,0 +1,12 @@
+// function vaarKhao() {
+//   console.log("Hand wash kore aso");
+//   console.log(`Boso`);
+//   console.log("Khao");
+// }
+
+function square(num) {
+  // console.log(num);
+  console.log(num * num);
+}
+
+square(50);
